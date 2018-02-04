720272062f6a7e2bbde8d9400ad2ae23bc0a41ef
aliba <baherami@gmail.com>
2018-02-04 17:38:52 +0200

    Added permissions logics for the user API

---
+++ src/webshop_project/webshop_app/permissions.py
@@ -0,0 +1,26 @@
+# import django permissions
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """Allow users to only edit their own profile"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check if user edits their own profile"""
+
+        #check if request is a safe request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # if it is not a safe method, the id should be same as the user id.
+        return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """ check if user is updating their status"""
+
+        if request.method is permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
