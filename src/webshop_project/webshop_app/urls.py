f0f43aff2bff375c811e4a70194b25a43e2e4718
aliba <baherami@gmail.com>
2018-02-04 17:33:53 +0200

    Added routing logics for login API

---
+++ src/webshop_project/webshop_app/urls.py
@@ -0,0 +1,15 @@
+# This is for adding view to our Hello API
+from django.conf.urls import url
+# adding include
+from django.conf.urls import include
+
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register('profile', views.UserProfileViewSet)
+router.register('Login', views.LoginViewSet, base_name='login')
+urlpatterns = [
+    url(r'',include(router.urls))
+]
