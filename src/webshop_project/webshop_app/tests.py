46f30ac40ab0b40ea57eae3891e4de6792f2072b
aliba <baherami@gmail.com>
2018-02-05 22:52:05 +0200

    Added first test unit

--- src/webshop_project/webshop_app/tests.py
+++ src/webshop_project/webshop_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
 # Create your tests here.
+class LoginTestCase(TestCase):
+    def test_a_new(self):
+        client = APIClient()
+        x=client.login(email='aliba@metropolia.fi', password='1234')
+        #token = Token.objects.get(user__email='aliba@metropolia.fi')
+        #client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        print("hello",x)
+        client.logout()
